77d9f65cec80d6821c9336b230ffbaaf61869f6b
Vijaym452 <vijaymunuswamy415@gmail.com>
2024-02-10 04:49:43 +0000

    vijaynxttube

---
+++ src/components/HomeVideoCard/index.js
@@ -0,0 +1,62 @@
+import {Link} from 'react-router-dom'
+import {formatDistanceToNow} from 'date-fns'
+
+import ThemeAndVideoContext from '../../context/ThemeAndVideoContext'
+
+import {
+  ListItem,
+  ThumbNailImage,
+  VideoDetails,
+  ProfileImage,
+  ContentSection,
+  Title,
+  ChannelName,
+  ViewsAndDate,
+  Dot,
+} from './styledComponents'
+
+import './index.css'
+
+const HomeVideoCard = props => {
+  const {video} = props
+  const {
+    id,
+    title,
+    thumbnailUrl,
+    viewCount,
+    publishedAt,
+    name,
+    profileImageUrl,
+  } = video
+
+  const publishedTimeToNow = formatDistanceToNow(new Date(publishedAt))
+
+  return (
+    <ThemeAndVideoContext.Consumer>
+      {value => {
+        const {isDarkTheme} = value
+        const textColor = isDarkTheme ? '#f9f9f9' : '#231f20'
+
+        return (
+          <Link to={`/videos/${id}`} className="link">
+            <ListItem>
+              <ThumbNailImage src={thumbnailUrl} alt="video thumbnail" />
+              <VideoDetails>
+                <ProfileImage src={profileImageUrl} alt="channel logo" />
+                <ContentSection>
+                  <Title color={textColor}>{title}</Title>
+                  <ChannelName color={textColor}>{name}</ChannelName>
+                  <ViewsAndDate color={textColor}>
+                    {viewCount} views<Dot> &#8226; </Dot> {publishedTimeToNow}
+                  </ViewsAndDate>
+                </ContentSection>
+              </VideoDetails>
+            </ListItem>
+          </Link>
+        )
+      }}
+    </ThemeAndVideoContext.Consumer>
+  )
+}
+
+export default HomeVideoCard
